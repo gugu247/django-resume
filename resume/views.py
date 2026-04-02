c359de9f51cac84a1c624d0e35dcda37b9098e16
gugu247 <gxleb80@gmail.com>
2026-04-02 22:00:03 +0300

    '2'

--- resume/views.py
+++ resume/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Profile,Education,Skill
 # Create your views here.
+
+def resume_view(request):
+    profile = get_object_or_404(Profile, pk=1)
+    context = {
+        "profile": profile,
+        "experiences": profile.experiences.all(),
+        "education": profile.education.all(),
+        "skill": profile.skill.all(),
+    }
+    return render(request, template_name:'resume/resume.html', context)
